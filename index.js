73f9c77228a76823855e61381b373be8d0e66a1f
jesterzhou <jesterzhou2004@gmail.com>
2023-04-15 23:42:09 -0400

    factored js code, removed unnecessary code

--- index.js
+++ index.js
@@ -1,26 +1,22 @@
 //date and time
-const options = { 
-    year: 'numeric', 
-    month: 'numeric', 
-    day: 'numeric'
-    
-}
 
 let date = new Date()
-const local_date = date.toLocaleDateString("en-US", options)
-let timezone = date.toString()
-timezone = (timezone[35] + timezone[43] + timezone[52])
+const local_date = date.toLocaleDateString("en-US")
+
+let timezone = date.toTimeString()
+timezone = (timezone[19] + timezone[27] + timezone[36])
 
 function clock () {
     let clock = new Date() 
     let time = clock.toLocaleTimeString("en-US", {timeZone: "America/New_York"});
-    document.getElementById("clock").innerHTML = time + "\n" + timezone;
+    document.getElementById("clock").innerHTML = time + "\n " + timezone;
+
 
 }
 
-
 window.onload = () => {
     document.getElementById("date").innerHTML = local_date;
+    clock()
+    setInterval(clock,1000)
 
-    setInterval(clock, 1000)
 }
